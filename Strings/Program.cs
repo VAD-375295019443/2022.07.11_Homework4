c2027e3d2421de19713584201f937e2fd9e2d598
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-07-10 11:53:22 +0300

    Changes

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -11,47 +11,20 @@ namespace Strings
 
             if (File.Exists(strPath) == true) //Если файл существует.
             {
-                string strText = "2.6 Мама ела, кашу... Папа т.п. 2.5 ел мед! 568...";
+                string strText =  File.ReadAllText(strPath); //Читаем данные из файла в переменную.
 
-                    
-                    
-                //File.ReadAllText(strPath); //Читаем данные из файла в переменную.
-                //Console.WriteLine(strText.Length);
+                string strPattern = @"((\.\.\.)|[0-9]{1,}.[0-9]{1,}|[\w\W-[\?\!\.\s]])((т.п.|т.д.|[0-9]{1,}.[0-9]{1,})|[\w\W-[\?\!\.]])*(\.\.\.\""|\.\""|\!\""|\?\""|\.\.\.\'|\.\'|\!\'|\?\'|\.\.\.|[\.\!\?])";
 
+                MatchCollection strMatchCollection = Regex.Matches(strText, strPattern, RegexOptions.Multiline);
 
-
-
-                string text = strText;
-                //"It real sent your at. Amounted all shy set why followed declared? Repeated of endeavor mr position kindness offering ignorant so up. Simplicity are melancholy preference considered saw companions. Disposal on outweigh do speedily in on. Him ham although thoughts entirely drawings. Acceptance unreserved old admiration projection nay yet him. Lasted am so before on esteem vanity oh."; //текст на разбор
-                //string pattern = @"[^.?!]{31,}(?=[.?!])"; //шаблон регулярного выражения
-
-
-                string pattern = @"((\.\.\.)|[0-9]{1,}.[0-9]{1,}|[\w\W-[\?\!\.\s]])((т.п.|т.д.|[0-9]{1,}.[0-9]{1,})|[\w\W-[\?\!\.]])*(\.\.\.\""|\.\.\.|[\.\!\?])";
-
-
-                //@"[^\s]((т.п.|т.д.|[0-9]{1,}.[0-9]{1,})|[^.!?])*((\.\.\.)|[.!?])";
-
-                //+++++    @"[^\s][0-9а-яА-Я\s,:;]((т.п.|т.д.)|[^.!?])*[\.!?]";\s,:;
-
-                //@"[A-Z|А-Я|0-9|a-z|а-я].*[^?!.][.!?]";
-                //@"[A-Z|А-Я|0-9]((т.п.|т.д.|пр.)|[^?!.]|\([^\)]*\))*[.?!]";
-
-                //@"^[A-Z|А-Я|0-9][.?!]$";
-
-
-                //@"[A-Z|А-Я|0-9] ((т.п.|т.д.|пр.) | [^?!.\(] | \([^\)]*\))*      [.?!]";
-
-
-
-                MatchCollection mc = Regex.Matches(text, pattern, RegexOptions.Multiline);
-
-                Console.WriteLine($"ВСЕГО {mc.Count}");
-
-                for (int i = 0; i <= mc.Count - 1; i++)
+                for (int i = 0; i <= 300; i++)
                 {
-                    Console.WriteLine(mc[i].Value);
+                    Console.WriteLine(strMatchCollection[i].Value);
 
 
+                    
+                    
+                    
                     //Console.WriteLine("{0} => {1}", mc[i].Groups[0].Value.Length, mc[i].Groups[0].Value);
                 }
                 //Console.ReadKey();
